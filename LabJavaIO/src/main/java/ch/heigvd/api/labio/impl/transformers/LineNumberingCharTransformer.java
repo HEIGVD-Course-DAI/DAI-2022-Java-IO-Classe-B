74ae86629e50ac43dd8b2177e327d83eca48c99a
Bastian Chollet <bastian.chollet@mobi.ch>
2022-10-03 19:09:19 +0200

    feat: ✨ added line numbering transformer

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -1,5 +1,6 @@
 package ch.heigvd.api.labio.impl.transformers;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
@@ -18,10 +19,30 @@ import java.util.logging.Logger;
  */
 public class LineNumberingCharTransformer {
   private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+  private static final String carriageReturn = "\r";
+  private static final String endLine = "\n";
+  private boolean isFirstCharOfLine = true;
+  private int idLine = 1;
 
   public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (c.equals(carriageReturn)) {
+      return "";
+    }
+
+    String result = c;
+
+    if (isFirstCharOfLine) {
+      isFirstCharOfLine = false;
+      result = writeLineNumber() + c;
+    }
+
+    if (c.equals(endLine)) {
+      result += writeLineNumber();
+    }
+
+    return result;
+  }
+  private String writeLineNumber() {
+    return idLine++ + ". ";
   }
 }
