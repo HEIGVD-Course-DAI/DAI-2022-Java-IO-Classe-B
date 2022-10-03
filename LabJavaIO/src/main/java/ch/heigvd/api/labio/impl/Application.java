e8a2486252cd93a9aa18a87ea3cd352ca738a990
AlexisMts <alexismartins9@hotmail.com>
2022-10-03 20:26:25 +0200

    Quick fix in Application and Add FileExplorer

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
@@ -80,7 +80,7 @@ public class Application {
        *  Add the missing line which stores the content of the quote in a file with
        *  the name "quote-i.utf8" where 'i' is the number of the file.
        */
-      storeQuote(quote, "quote-i.utf8");
+      storeQuote(quote, "quote-" + i + ".utf8");
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
