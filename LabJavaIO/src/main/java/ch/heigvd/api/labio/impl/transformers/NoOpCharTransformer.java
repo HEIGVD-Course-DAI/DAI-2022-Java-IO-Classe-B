0091bbf8014540bbd84dfdd349efe4665fb85110
Flag <flagui1462@hotmail.com>
2022-10-03 16:14:17 +0200

    Test success for NoOp transformation

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/NoOpCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/NoOpCharTransformer.java
@@ -14,6 +14,8 @@ public class NoOpCharTransformer {
     public String transform(String c) {
         /* TODO: implement the transformation here.
          */
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        //We simpley return the same String since there is no operation
+        return c;
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
     }
 }
