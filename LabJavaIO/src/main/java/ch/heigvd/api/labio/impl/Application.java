c01efee50b5105a6cf7d7124c303d33ca0f5c51b
Greywind299792458 <71220948+Greywind299792458@users.noreply.github.com>
2022-10-03 15:02:31 +0100

    first commit to create the pull request

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
@@ -77,7 +77,10 @@ public class Application {
        *  quote in a text file (and for generating the directories based on the tags).
        *  Add the missing line which stores the content of the quote in a file with
        *  the name "quote-i.utf8" where 'i' is the number of the file.
+       *
+       * UPDATE : Elliot is trying to fix this
        */
+      storeQuote(quote, String.format("quote-%d.utf8", i));
 
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
