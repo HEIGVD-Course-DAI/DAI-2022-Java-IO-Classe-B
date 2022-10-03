c78f48f11c2955b9c5dd9aa9e93e2356ac722d26
malo-romano <malo.romano@gmail.com>
2022-10-03 18:48:06 +0200

    File explorer is working

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
@@ -30,20 +30,6 @@ public class FileTransformer {
          * a character transformer to transform the character before writing it to the output.
          */
 
-        /* TODO: first start with the NoOpCharTransformer which does nothing.
-         *  Later, replace it by a combination of the UpperCaseCharTransformer
-         *  and the LineNumberCharTransformer.
-         */
-        // ... transformer = ...
-
-        /* TODO: implement the following logic here:
-         *  - open the inputFile and an outputFile
-         *    Use UTF-8 encoding for both.
-         *    Filename of the output file: <inputFile-Name>.out (that is add ".out" at the end)
-         *  - Copy all characters from the input file to the output file.
-         *  - For each character, apply a transformation: start with NoOpCharTransformer,
-         *    then later replace it with a combination of UpperCaseFCharTransformer and LineNumberCharTransformer.
-         */
         try {
             // Extracting file content
             String inputFileContent = Files.readString(inputFile.toPath());
