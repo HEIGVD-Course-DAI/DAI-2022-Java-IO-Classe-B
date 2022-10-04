8e37e7fd02f1101c9bc79d1ffdf9b6c45b57720e
Hugo <mrjokesheep@gmail.com>
2022-10-04 21:59:04 +0200

    Passed all the transformers tests

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -1,5 +1,6 @@
 package ch.heigvd.api.labio.impl.transformers;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
@@ -17,11 +18,20 @@ import java.util.logging.Logger;
  * @author Olivier Liechti, Juergen Ehrensberger
  */
 public class LineNumberingCharTransformer {
-  private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+  private static final Logger LOG = Logger.getLogger(LineNumberingCharTransformer.class.getName());
+  private boolean isFirstCharacterSent = true;
+  private int count = 0;
 
   public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String ret = c;
+    if (isFirstCharacterSent) {
+      isFirstCharacterSent = false;
+      ret = ++count + ". " + ret;
+    }
+    if (Objects.equals(c, "\r"))
+      return "";
+    if (Objects.equals(c, "\n"))
+      ret = ret + ++count + ". ";
+    return ret;
   }
 }
