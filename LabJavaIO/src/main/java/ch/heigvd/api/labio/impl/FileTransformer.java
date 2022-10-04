fd691eed011e885e0c8fc8c7ea8f2e1d9598028c
azzen <6373020+azzen@users.noreply.github.com>
2022-10-04 08:45:39 +0200

    Remove todos

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
@@ -30,22 +30,8 @@ public class FileTransformer {
      * Before writing each character to the output file, the transformer calls
      * a character transformer to transform the character before writing it to the output.
      */
-
-    /* TODO: first start with the NoOpCharTransformer which does nothing.
-     *  Later, replace it by a combination of the UpperCaseCharTransformer
-     *  and the LineNumberCharTransformer.
-     */
-    // ... transformer = ...
       LineNumberingCharTransformer transformer1 = new LineNumberingCharTransformer();
       UpperCaseCharTransformer transformer2 = new UpperCaseCharTransformer();
-    /* TODO: implement the following logic here:
-     *  - open the inputFile and an outputFile
-     *    Use UTF-8 encoding for both.
-     *    Filename of the output file: <inputFile-Name>.out (that is add ".out" at the end)
-     *  - Copy all characters from the input file to the output file.
-     *  - For each character, apply a transformation: start with NoOpCharTransformer,
-     *    then later replace it with a combination of UpperCaseFCharTransformer and LineNumberCharTransformer.
-     */
     try {
         FileReader fr = new FileReader(inputFile, StandardCharsets.UTF_8);
         FileWriter fw = new FileWriter(inputFile + ".out", StandardCharsets.UTF_8);
