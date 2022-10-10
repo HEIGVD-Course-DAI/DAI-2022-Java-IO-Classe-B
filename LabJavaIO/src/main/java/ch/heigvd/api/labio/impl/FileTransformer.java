11f92d354f7f7e94c220601c0909fba308e9516a
CSharper63 <maxime.chantemargue@heig-vd.ch>
2022-10-10 15:33:12 +0200

    transform method implemented

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
@@ -1,6 +1,10 @@
 package ch.heigvd.api.labio.impl;
 
+import ch.heigvd.api.labio.impl.transformers.NoOpCharTransformer;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,37 +16,45 @@ import java.util.logging.Logger;
  * @author Juergen Ehrensberger
  */
 public class FileTransformer {
-  private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
 
-  public void transform(File inputFile) {
-    /*
-     * This method opens the given inputFile and copies the
-     * content to an output file.
-     * The output file has a file name <inputFile-Name>.out, for example:
-     *   quote-2.utf --> quote-2.utf.out
-     * Both files must be opened (read or write) with encoding "UTF-8".
-     * Before writing each character to the output file, the transformer calls
-     * a character transformer to transform the character before writing it to the output.
-     */
+    public void transform(File inputFile) {
+        /*
+         * This method opens the given inputFile and copies the
+         * content to an output file.
+         * The output file has a file name <inputFile-Name>.out, for example:
+         *   quote-2.utf --> quote-2.utf.out
+         * Both files must be opened (read or write) with encoding "UTF-8".
+         * Before writing each character to the output file, the transformer calls
+         * a character transformer to transform the character before writing it to the output.
+         */
 
-    /* TODO: first start with the NoOpCharTransformer which does nothing.
-     *  Later, replace it by a combination of the UpperCaseCharTransformer
-     *  and the LineNumberCharTransformer.
-     */
-    // ... transformer = ...
+        /* TODO: first start with the NoOpCharTransformer which does nothing.
+         *  Later, replace it by a combination of the UpperCaseCharTransformer
+         *  and the LineNumberCharTransformer.
+         */
+        // ... transformer = ...
 
-    /* TODO: implement the following logic here:
-     *  - open the inputFile and an outputFile
-     *    Use UTF-8 encoding for both.
-     *    Filename of the output file: <inputFile-Name>.out (that is add ".out" at the end)
-     *  - Copy all characters from the input file to the output file.
-     *  - For each character, apply a transformation: start with NoOpCharTransformer,
-     *    then later replace it with a combination of UpperCaseFCharTransformer and LineNumberCharTransformer.
-     */
-    try {
+        NoOpCharTransformer transformer = new NoOpCharTransformer();
 
-    } catch (Exception ex) {
-      LOG.log(Level.SEVERE, "Error while reading, writing or transforming file.", ex);
+        /* TODO: implement the following logic here:
+         *  - open the inputFile and an outputFile
+         *    Use UTF-8 encoding for both.
+         *    Filename of the output file: <inputFile-Name>.out (that is add ".out" at the end)
+         *  - Copy all characters from the input file to the output file.
+         *  - For each character, apply a transformation: start with NoOpCharTransformer,
+         *    then later replace it with a combination of UpperCaseFCharTransformer and LineNumberCharTransformer.
+         */
+        //TODO verify behavior !!
+        if (inputFile.exists() && inputFile.isFile()) {
+            try {
+                FileInputStream fis = new FileInputStream(inputFile);
+                File newFile = new File(inputFile.getParent(), inputFile.getName() + ".out");
+                FileOutputStream fos = new FileOutputStream(newFile);
+                fis.readAllBytes();
+            } catch (Exception ex) {
+                LOG.log(Level.SEVERE, "Error while reading, writing or transforming file.", ex);
+            }
+        }
     }
-  }
 }
