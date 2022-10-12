75f464b571dc097356913056298d9d0d408ca3f9
yvancochet <yvan.cochet@heig-vd.ch>
2022-10-12 20:46:27 +0200

    Add file sort

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
@@ -2,6 +2,8 @@ package ch.heigvd.api.labio.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * The FileExplorer performs an exploration of the file system. It
@@ -18,9 +20,9 @@ public class FileExplorer {
 
         //Solution found here : https://stackoverflow.com/questions/2056221/recursively-list-files-in-java
         File[] files = rootDirectory.listFiles();
-        if(files == null)
+        if(files == null || files.length == 0)
             return;
-
+        Arrays.sort(files, Comparator.comparing(File::getName));
         for (File f : files) {
             if (f.isDirectory())
                 explore(f);
